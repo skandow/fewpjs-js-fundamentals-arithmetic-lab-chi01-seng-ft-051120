402e4de0ae6c8085e25272e95765b0bb7b1143bc
skandow <kando1sr@gmail.com>
2020-06-29 10:00:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2; 
+
+let random = Math.floor(Math.random() * 8) + 1;
+
+const num3 = 68;
+const num4 = 8;
+
+const mod = num3 % num4;
+
+const max = Math.max(2, 4, 6, 8, 10, 12, 14, 16, 18, 20)
